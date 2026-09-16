592b9de3c6d6f1adc154f6d4afc118e55a2e5e98
ss4url-afk <ss4url@gmail.com>
2026-09-15 19:32:46 -0500

    Create ss4url-afk.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/ss4url-afk.py
@@ -0,0 +1,26 @@
+# https://python.org
+# Comentario en una linea 
+"""
+Esto tambien es
+un comentario 
+varias lineas
+"""
+my_variable = "Mi variable"
+my_variable = "Nuevo de valor de mi variable"
+
+MY_CONSTANT = "Mi constante" #por convencion
+my_int = 1
+my_float = 1.5
+my_bool = True
+my_bool = False
+my_string = "Mi cadena de texto"
+my_other_string ='Mi otracadena de texto'
+
+print("¡Hola, Python!)
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
+      
+  
